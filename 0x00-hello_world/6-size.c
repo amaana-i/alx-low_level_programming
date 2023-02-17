61056050990b8c769061610413ce71bee85b22ab
amaana-i <amaanaishaquehayfeen@gmail.com>
2023-02-16 21:03:11 -0800

    Task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,6 @@
 
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
 
 	printf("Size of a char: %d byte(S)\n", sizeof(char));
 	printf("Size of an int: %d byte(S)\n", sizeof(int));
