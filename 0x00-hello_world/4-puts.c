a2fbbbe3070b377b88c74ed5141ed47019193446
amaana-i <amaanaishaquehayfeen@gmail.com>
2023-02-16 19:38:20 -0800

    Task4 hello world first C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
+/**
  * main - Printing a line line code with put function
  *
  * Return:0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
