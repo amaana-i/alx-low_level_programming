33ebd4c9f3025bb546606a578cf69e9e00427ed9
amaana-i <amaanaishaquehayfeen@gmail.com>
2023-02-22 02:37:52 -0800

    Task1 Printing alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - alphabet print in lowercase
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * _putchar - Writes a character to stdout
+ *
+ * @c: The character to be written
+ *
+ * Return: On success, the character written is returned.
+ * On error, -1 is returned.
+ */
 int _putchar(char c)
+void print_alphabet(void);
 
 #endif
