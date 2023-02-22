d0bed34e73083edac0c77048be02931c22ee889e
amaana-i <amaanaishaquehayfeen@gmail.com>
2023-02-22 05:12:26 -0800

    Task0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * _putchar - Writes a character to stdout
- *
- * @c: The character to be written
- *
- * Return: On success, the character written is returned.
- * On error, -1 is returned.
- */
-int _putchar(char c)
-void print_alphabet(void);
+int _putchar(char c);
 
 #endif
