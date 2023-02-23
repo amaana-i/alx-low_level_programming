88fdd6d5e875e7a86bcd4dc494f1cc5e7cd4b5b3
amaana-i <amaanaishaquehayfeen@gmail.com>
2023-02-22 16:40:45 -0800

    task13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Printing all numbers below 1024 that are multiples of 3 or 5
+ * main - Prints all numbers below 1024 that are multiples of 3 or 5
  * Return: Returns 0 when successful
  */
 
 int main(void)
 {
-	int x, y;
+	int a, b;
 
-	for (x = 1; x < 1024; x++)
+	for (a = 1; a < 1024; a++)
 	{
-		if ((x % 3) == 0 || (x % 5) == 0)
-			y += x;
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
 	}
 	printf("%d\n", b);
 	return (0);
